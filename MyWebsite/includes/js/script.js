6d51162e83c7186bafed981dc3104831447e6e87
DeepakBalyan23 <deepakbalyan23@gmail.com>
2018-06-04 16:40:21 +0530

    jQuery_tooltip_popover

--- MyWebsite/includes/js/script.js
+++ MyWebsite/includes/js/script.js
@@ -13,4 +13,9 @@ $(function () {
 		e.preventDefault();
 		$('#successAlert').slideDown();
 	});
+	$('a.pop').click(function(e){
+		e.preventDefault();
+	});
+	$('a.pop').popover();
+	$('[rel="tooltip"]').tooltip();
 });
