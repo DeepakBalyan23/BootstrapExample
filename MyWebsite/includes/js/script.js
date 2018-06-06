3e6195f664e2a23a8530c8c05eb597f37fcb1f22
DeepakBalyan23 <deepakbalyan23@gmail.com>
2018-06-06 10:01:34 +0530

    removed_extra_js

--- MyWebsite/includes/js/script.js
+++ MyWebsite/includes/js/script.js
@@ -8,10 +8,3 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
-$(function () {
-	$('a.pop').click(function(e){
-		e.preventDefault();
-	});
-	$('a.pop').popover();
-	$('[rel="tooltip"]').tooltip();
-});
